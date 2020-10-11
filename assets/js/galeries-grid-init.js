82cea3dd9402d7fa4010e7b1f4d5c98264d73dc1
Leonardo 'WildLeoKnight' Lourenco <leonardo-lourenco@hotmail.com>
2020-10-11 17:23:12 +0100

    Css and Mansonry js fixes,  Settable Social links
    I also cleaned some variables and unused pages

---
+++ assets/js/galeries-grid-init.js
@@ -0,0 +1,12 @@
+FlexMasonry.init('.list-galeries', {
+    responsive: true,
+    breakpointCols: {
+        'min-width: 1920px': 7,
+        'min-width: 1500px': 6,
+        'min-width: 1200px': 5,
+        'min-width: 992px': 4,
+        'min-width: 768px': 4,
+        'min-width: 576px': 3,
+        'min-width: 250px': 2,
+    }
+});
--- assets/js/post-grid-init.js
+++ assets/js/post-grid-init.js
@@ -1,7 +1,12 @@
 FlexMasonry.init('.list-posts', {
     responsive: true,
     breakpointCols: {
-        'min-width: 1500px': 3,
-        'min-width: 700px': 2,
+        'min-width: 1920px': 4,
+        'min-width: 1500px': 4,
+        'min-width: 1200px': 3,
+        'min-width: 992px': 3,
+        'min-width: 768px': 2,
+        'min-width: 576px': 2,
+        'min-width: 250px': 1,
     }
 });
