bae45f5c5879052e6bfd96dd16abec69ad2c9111
Leonardo 'WildLeoKnight' Lourenco <leonardo-lourenco@hotmail.com>
2020-10-19 14:13:55 +0100

    Sidebar and Mobile Navigation

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -9,3 +9,32 @@ function scrollFunction() {
     document.getElementById("sidebar").classList.remove('sidebar-minimised');
   }
 }
+  
+var acc = document.getElementsByClassName("mobileSideNav-Accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+acc[i].addEventListener("click", function() {
+    this.classList.toggle("mobileSideNav-Accordion-active");
+    var accList = this.nextElementSibling;
+    if (accList.classList.contains("submenu")) {
+        accList.classList.remove("submenu");
+        accList.classList.add("submenu-active");
+    } else {
+        accList.classList.add("submenu");
+        accList.classList.remove("submenu-active");
+    }
+});
+}
+
+function OpenCloseMobileNav() {
+    let mobileNavBar = document.getElementById("mobileSideNav");
+    let body = document.getElementsByTagName("BODY")[0]
+    if (mobileNavBar.classList.contains("open")){
+        mobileNavBar.classList.remove("open");
+        body.classList.remove("mobileSideNav-active")
+    }else{
+        mobileNavBar.classList.add("open");
+        body.classList.add("mobileSideNav-active")
+    }
+}
