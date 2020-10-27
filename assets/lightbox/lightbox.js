8c7e3123a14d056e63fecf89907f778da1dd2bd4
Leonardo 'WildLeoKnight' Lourenco <leonardo-lourenco@hotmail.com>
2020-10-27 00:08:59 +0000

    Fixed lightbox galleries

--- assets/lightbox/lightbox.js
+++ assets/lightbox/lightbox.js
@@ -42,8 +42,8 @@ function setGallery(el) {
     elements.forEach(element => {
         element.classList.remove('gallery');
 	});
-	if(el.closest('ul, p')) {
-		var link_elements = el.closest('ul, p').querySelectorAll("a[class*='lightbox-']");
+	if(el.closest('.list-gallery')) {
+		var link_elements = el.closest('.list-gallery').querySelectorAll("a[class*='lightbox-']");
 		link_elements.forEach(link_element => {
 			link_element.classList.remove('current');
 		});
